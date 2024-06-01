1c2f08edcf889219ec65caa7827c198b4876a6b4
deepak <nadcabdeepak@gmail.com>
2024-06-01 17:34:09 +0530

    done

--- src/components/Details/Viewdetails.jsx
+++ src/components/Details/Viewdetails.jsx
@@ -12,11 +12,10 @@ import { FaArrowRightLong } from "react-icons/fa6";
 import Footer from "../Footer";
 
 function Viewdetails() {
-
   const [userDetail, setUserDetail] = useState(null);
-  const [responseIncome, setResponseIncome] = useState([]);
+  const [responseIncome, setResponseIncome] = useState(null);
   const [claim, setClaim] = useState(null);
-  const [tokenAddress,setTokenAddress]=useState(null)
+  const [tokenAddress, setTokenAddress] = useState(null);
   const [tokenName, setTokenName] = useState(null);
   const [tokenSymbol, setTokenSymbol] = useState(null);
   const [tokenDecimal, setTokenDecimal] = useState(null);
@@ -28,6 +27,8 @@ function Viewdetails() {
     fetchClaimDetails(url1).then((res) => {
       if (res) {
         setUserDetail(res.data[0]);
+      } else {
+        setUserDetail(null);
       }
     });
     const url2 = `http://localhost/gccApi/sponserincome.php?user=${tokenValue[0]}`;
@@ -45,15 +46,14 @@ function Viewdetails() {
 
     getTokenDetail(tokenValue[0]).then((res) => {
       if (res) {
-        setTokenAddress(res)
+        setTokenAddress(res);
       }
-    })
+    });
   }, [tokenValue]);
 
   // token Address
 
-  useEffect(()=>{
-
+  useEffect(() => {
     getName(tokenAddress).then((res) => {
       if (res) {
         setTokenName(res);
@@ -66,25 +66,22 @@ function Viewdetails() {
     });
     getDecimal(tokenAddress).then((res) => {
       if (res) {
-        console.log(res,'decimal')
         setTokenDecimal(res);
       }
     });
     getTotalSupply(tokenAddress).then((res) => {
       if (res) {
-        console.log(res,'supply')
         setTokenSupply(res);
       }
     });
-
-  },[tokenAddress])
+  }, [tokenAddress]);
 
   return (
     <>
       <div className="container mb-5">
         <div className="row mt-5">
-          <div className="col-md-6">
-            {userDetail && (
+          {userDetail && (
+            <div className="col-md-12 col-lg-6 mb-3">
               <div class="card" style={{ backgroundColor: "#F1F3F5" }}>
                 <div class="card-body">
                   <h5 class="card-title text-center">User Detail</h5>
@@ -114,10 +111,12 @@ function Viewdetails() {
                   </p>
                 </div>
               </div>
-            )}
-          </div>
-          <div className="col-md-6">
-            {claim && (
+            </div>
+          )}
+          {claim?.userReferalAddress !=
+            "0x0000000000000000000000000000000000000000" &&
+          claim?.userReferalAddress ? (
+            <div className="col-md-12 col-lg-6">
               <div
                 class="card"
                 style={{ height: "18.5rem", backgroundColor: "#F1F3F5" }}
@@ -144,38 +143,42 @@ function Viewdetails() {
                   </p>
                 </div>
               </div>
-            )}
-          </div>
-          <div className="col-md-6">
-            {tokenAddress &&
-            <div className="card mt-3" style={{ backgroundColor: "#F1F3F5" }}>
-              <div class="card-body pe-1">
-                <h5 class="card-title text-center">Token Details</h5>
-                <p class="card-text fw-bold mt-3">
-                  Token Address :
-                  <span className="fw-normal">{tokenAddress}</span>
-                </p>
-                <p class="card-text fw-bold">
-                  Name :  <span className="fw-normal">{tokenName}</span>
-                </p>
-                <p class="card-text fw-bold">
-                  Symbol : 
-                  <span className="fw-normal">{tokenSymbol}</span>
-                </p>
-                <p class="card-text fw-bold">
-                  Decimal : 
-                  <span className="fw-normal">{Number(tokenDecimal)}</span>
-                </p>
-                <p class="card-text fw-bold">
-                  Total Supply :  
-                  <span className="fw-normal">{tokenSupply && (Number(tokenSupply)/1e54).toLocaleString()}</span>
-                </p>
+            </div>
+          ) : (
+            ""
+          )}
+          {tokenAddress && (
+            <div className="col-md-12 col-lg-6 mb-5">
+              <div className="card mt-3" style={{ backgroundColor: "#F1F3F5" }}>
+                <div class="card-body pe-1">
+                  <h5 class="card-title text-center">Token Details</h5>
+                  <p class="card-text fw-bold mt-3">
+                    Token Address :
+                    <span className="fw-normal">{tokenAddress}</span>
+                  </p>
+                  <p class="card-text fw-bold">
+                    Name : <span className="fw-normal">{tokenName}</span>
+                  </p>
+                  <p class="card-text fw-bold">
+                    Symbol :<span className="fw-normal">{tokenSymbol}</span>
+                  </p>
+                  <p class="card-text fw-bold">
+                    Decimal :
+                    <span className="fw-normal">{Number(tokenDecimal)}</span>
+                  </p>
+                  <p class="card-text fw-bold">
+                    Total Supply :
+                    <span className="fw-normal">
+                      {tokenSupply &&
+                        (Number(tokenSupply) / 1e54).toLocaleString()}
+                    </span>
+                  </p>
+                </div>
               </div>
             </div>
-}
-          </div>
+          )}
 
-          <div className="view-details mt-5">
+          <div className="view-details col-md-12 col-lg-12 table-responsive">
             <table class="table">
               <thead>
                 <tr>
@@ -199,34 +202,44 @@ function Viewdetails() {
                 </tr>
               </thead>
               <tbody>
-                {responseIncome.map((data) => (
+                {responseIncome ? (
+                  responseIncome.map((data) => (
+                    <tr>
+                      <td scope="row">{data.level}</td>
+                      <td>{data.from}</td>
+                      {/* <td>{data.to}</td> */}
+                      <td>{Math.round(data.amount / 1e18)}</td>
+                      <td>
+                        <a
+                          href={`https://testnet.bscscan.com/tx/${data.transaction_id}`}
+                          style={{ color: "black"}}
+                          className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover link-dark"
+                          id='trnsId'
+                          target="_blanck"
+                        >
+                          {data.transaction_id.slice(1, 10) +
+                            "......." +
+                            data.transaction_id.slice(-5)}
+                          <FaArrowRightLong className="ms-1" />
+                        </a>
+                      </td>
+                      <td>{new Date(data.time * 1000).toLocaleDateString()}</td>
+                    </tr>
+                  ))
+                ) : (
                   <tr>
-                    <td scope="row">{data.level}</td>
-                    <td>{data.from}</td>
-                    {/* <td>{data.to}</td> */}
-                    <td>{Math.round(data.amount / 1e18)}</td>
-                    <td>
-                      <a
-                        href={`https://testnet.bscscan.com/tx/${data.transaction_id}`}
-                        style={{ color: "black", textDecoration: "none" }}
-                        target="_blanck"
-                      >
-                        {data.transaction_id.slice(1, 10) +
-                          "......." +
-                          data.transaction_id.slice(-5)}
-                        <FaArrowRightLong className="ms-1" />
-                      </a>
+                    <td colSpan={5} className="text-center fw-bold">
+                      Record Not Found
                     </td>
-                    <td>{new Date(data.time * 1000).toLocaleDateString()}</td>
                   </tr>
-                ))}
+                )}
               </tbody>
             </table>
             {/* <Pagination count={5} variant="outlined" shape="rounded" /> */}
           </div>
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </>
   );
 }
